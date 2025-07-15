1b113e470fa662ffb921a7ee6bedff2f7291f93b
sjtuzl <zhling@gmail.com>
2025-07-15 08:56:57 +0800

    My first commit from IDEA IDE.

---
+++ src/com/charlie/test/Test1.java
@@ -0,0 +1,11 @@
+package com.charlie.test;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int i = 10;
+        i++;
+        System.out.println("hello world1");
+    }
+}
